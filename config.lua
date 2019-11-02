2dcb571d87a98a844f99a9dc56173140c6a453fa
Why you want to know <48079270+TypicalTheDev@users.noreply.github.com>
2019-11-01 20:56:13 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,18 @@
+config={} 
+config.time = 5 -- 5 is 5 min you can put what you want ex 3 would be 3 min this would be how long it would send each message
+config.name = 'TXHC' -- change this to your server name 
+config.enabled = true 
+config.messages = {
+    'Have a suggestion for the server? Feel free to post on the forums at ^2http://forum.harriscountyrp.com/forum/^0 or in the Discord.',      
+    'Think you have what it takes to be apart of our ^2Staff Team^0? Apply at ^2https://txhc-rp.com/',
+    'Looking to join a department? apply on our website at ^2https://txhc-rp.com/^0.',
+    'Roleplaying as a ^2civilian^0? You must use out MDT/CAD at ^2https://therocketcad.com/invite?code=i1u5E1^0. Failure to do so may result in a warning, kick, or ban.',
+    'Make sure to read our rules at ^2https://txhc-rp.com/rules^0. Failure to follow the rules may result in a warning, kick, or ban.',
+    'Want to talk to other civilians? Join our Discord at https://discord.gg/36wzVPu^',
+    'Want to stay up to date? Join our ^4Discord^0 server (^2https://discord.gg/36wzVPu^0). Our ^4Discord^0 is used for text communication only!',
+}
+config.tfmsg = {
+[true] = 'Activated',
+[false] = 'Deactivated' 
+
+}
